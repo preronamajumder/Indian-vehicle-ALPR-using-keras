d18e47122350da6220f40dd9db7acdcd043d97af
Prerona Majumder <preronamajumder@gmail.com>
2021-02-28 15:42:35 +0530

    initial commit

---
+++ main_alpr.py
@@ -0,0 +1,184 @@
+import os
+import sys
+import cv2
+from skimage import measure
+from skimage.measure import regionprops
+import numpy as np
+from keras.models import load_model
+import configparser
+
+
+def get_binary_image(img):
+    binary = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 29, 2)
+    inv_binary = 255 - binary
+    return inv_binary
+
+def get_sorted_char_list(cropped_char):
+    char_keys = sorted(cropped_char.keys())
+    char_list = []
+    for x in char_keys:
+        char_list.append(cropped_char[x])
+        # cv2.imshow("lp", cropped_char[x])
+        # cv2.waitKey(0)
+    return char_list
+
+def character_segmentation(img):
+    character_dimensions = (0.3*img.shape[0], 0.9*img.shape[0], 0.02*img.shape[1], 0.5*img.shape[1])
+    min_height, max_height, min_width, max_width = character_dimensions
+
+    labelled_img = measure.label(img)
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (2, 1))
+
+    cropped_char = {}
+    
+    for regions in regionprops(labelled_img):
+        y1, x1, y2, x2 = regions.bbox
+        region_height = y2 - y1
+        region_width = x2 - x1
+
+        if min_height < region_height < max_height and min_width < region_width < max_width \
+            and y2 < 0.95*img.shape[0] and y1 > 0.05*img.shape[0] and region_height >= region_width \
+            and x2 < 0.98*img.shape[1] and x1 > 0.05*img.shape[1]:
+
+            # print(x1, y1, x2, y2)
+            roi = img[max(y1-2, 0):min(y2+2, img.shape[0]), max(x1-2, 0):min(x2+2, img.shape[1])]
+            roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
+            roi = cv2.cvtColor(255-roi, cv2.COLOR_GRAY2RGB)
+
+            xc = x1 + region_width / 2
+            # yc = y1 + region_height / 2
+            cropped_char[int(xc)] = roi
+
+    #sort characters from left to right
+    char_list = get_sorted_char_list(cropped_char)
+
+    return char_list
+
+def get_characters(img):
+    try:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    except Exception as e:
+        print(e)
+    
+    img = get_binary_image(img)
+
+    cv2.imshow("lp", img)
+    cv2.waitKey(0)
+
+    char_list = character_segmentation(img)
+    print(len(char_list))
+    return char_list
+
+def load_lp_model(path):
+    model = load_model(path)
+    model.summary()
+    return model
+
+def load_classes(label_path):
+    with open(label_path, "r") as f:
+        classes = f.readlines()
+        classes = [c.strip() for c in classes]
+    print(len(classes))
+    return classes
+
+def preprocessing(char_list):
+    return [char/255 for char in char_list]
+
+def lp_recognition(char_list, model, classes):
+
+    #preprocessing
+    char_list = preprocessing(char_list)
+
+    #predict
+    y_preds = model.predict(np.array(char_list))
+    #print(np.round(y_preds[0], 3))
+    y_hat = [np.argmax(y) for y in y_preds]  # list of predictions
+    print(np.array(y_hat))
+
+    lp_number = [classes[x] for x in y_hat]
+    #TODO: check LP format 
+    lp_number = ''.join(lp_number)
+    
+    return lp_number
+
+
+def check_config(data):
+    if 'model_path' not in data:
+        print("Model path not provided")
+        sys.exit()
+    if 'label_path' not in data:
+        print("Label path not provided")
+        sys.exit()
+    if 'is_folder' not in data:
+        print("is_folder not provided")
+        sys.exit()
+    if data['is_folder'] == 0:
+        if 'image_path' not in data:
+            print("Image path not provided")
+            sys.exit()
+    else:
+        if 'folder_path' not in data:
+            print("Image path not provided")
+            sys.exit()
+    
+def read_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    config.sections()
+
+    data = {}
+    for key in config['DEFAULT']:
+        if key == "model_file":
+            data['model_path'] = config.get('DEFAULT', key)
+
+        if key == "label_file":
+            data['label_path'] = config.get('DEFAULT', key)
+
+        if key == "is_folder":
+            data['is_folder'] = bool(config.getint('DEFAULT', key))
+
+        if key == "image_path":
+            data['image_path'] = config.get('DEFAULT', key)
+
+        if key == "folder_path":
+            data['folder_path'] = config.get('DEFAULT', key)
+        
+        if key == "write_to_csv":
+            data['write_to_csv'] = config.get('DEFAULT', key)
+
+    check_config(data)
+    return data
+    
+def main():
+    data = read_config("alpr_config.txt")
+
+    #load model
+    model = load_lp_model(data['model_path'])
+    #load classes
+    classes = load_classes(data['label_path'])
+
+    if not data['is_folder']:
+        #read license plate image
+        img = cv2.imread(data['image_path'])
+        #character segmentation
+        char_list = get_characters(img)
+        #character recognition
+        number_plate = lp_recognition(char_list, model, classes)
+        print('LP Number: ', number_plate)
+
+    else:
+        #read license plate images
+        for files in os.listdir(data['folder_path']):
+            print(files)
+            if files.endswith(".jpg"):
+                img = cv2.imread(os.path.join(data['folder_path'],files))
+                #character segmentation
+                char_list = get_characters(img)
+                #character recognition
+                number_plate = lp_recognition(char_list, model, classes)
+
+                print('LP Number: ', number_plate)
+        
+
+if __name__ == "__main__":
+    main()
---
+++ train_alpr.py
@@ -0,0 +1,178 @@
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D
+from keras.optimizers import SGD, Adam
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import classification_report
+import os
+import cv2
+import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras import backend as K
+
+
+def load_data(dir):
+
+    image_list = []
+    labels = []
+    count = 0
+    for folder in sorted(os.listdir(dir)):
+        print(folder)
+        if len(folder) == 1:
+            #print('character name: ', folder)
+            for image in os.listdir(dir+'/'+folder):
+                if image.endswith(".jpg"):
+                    #print('character file name: ', image)
+                    img = cv2.imread(dir+'/'+folder+'/'+image)/255
+                    img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+                    image_list.append(img)
+                    labels.append(folder)
+                    count += 1
+                    print(count)
+
+    image_array = np.array(image_list)
+    d = {key: value for value, key in enumerate(set(labels))}
+    ls = [d[key] for key in labels]
+    ln = np.sort(list(set(labels)))     # all classes
+    l = (list(set(ls)))
+    class_mapping = {}
+    for i in range(len(ln)):
+        class_mapping[ln[i]] = l[i]
+
+    print(class_mapping)
+    return image_array, labels, class_mapping
+
+
+def preprocessing(image_array, y):
+    import random
+
+    random.seed(0)
+
+    # split into train and test set
+    X_train, X_test, y_train, y_test = train_test_split(image_array, y, test_size=0.25, random_state=42, stratify=y)
+    train_class = y_train
+    test_class = y_test
+
+    # reshaped into original shape (number of images, image height, image width, number of channels)
+    X_train = X_train.reshape(len(X_train), X_train[0].shape[0], X_train[0].shape[1], 3)
+    X_test = X_test.reshape(len(X_test), X_test[0].shape[0], X_test[0].shape[1], 3)
+
+    # categorical mapping for probabilities
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    return X_train, X_test, y_train, y_test, train_class, test_class
+
+
+def cnn(X_train):
+    model = Sequential()
+
+    model.add(Conv2D(8, (3, 3), padding='same', input_shape=(X_train[0].shape[0], X_train[0].shape[1], 3),
+                     activation='relu', kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(16, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    #model.add(Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     #bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    #model.add(Conv2D(64, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     #bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    #model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    #model.add(Conv2D(128, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     #bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    #model.add(Conv2D(128, (3, 3), padding='same', activation='relu', kernel_initializer='glorot_uniform',
+                     #bias_initializer='glorot_uniform'))
+    # model.add(BatchNormalization(axis=-1, momentum=0.99, epsilon=0.001))
+    #model.add(MaxPooling2D((2, 2), strides=(2, 2)))
+
+    model.add(Flatten())
+    #model.add(Dense(128, activation='relu', kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform'))
+    model.add(Dense(64, activation='relu', kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform'))
+    model.add(Dense(34, activation='softmax', kernel_initializer='glorot_uniform', bias_initializer='glorot_uniform'))
+
+    return model
+
+
+def load_lp_chars(dir):
+    image_list = []
+    count = 0
+    for image in sorted(os.listdir(dir)):
+        if image != '.DS_Store':
+            if image.endswith('.jpg') or image.endswith('.png'):
+                # print('character file name: ', image)
+                img = cv2.imread(dir + '/' + image)/255
+                img = cv2.resize(img, (28, 28), interpolation=cv2.INTER_AREA)
+                image_list.append(img)
+                count += 1
+                print(count)
+
+    image_array = np.array(image_list)
+
+    return image_array
+
+def main():
+    dir = './ocr_char'
+    image_array, labels, class_mapping = load_data(dir)
+    print(len(image_array))
+    print(image_array.shape)
+    print(len(labels))
+    inv_map = {v: k for k, v in class_mapping.items()}
+    print(inv_map)
+
+    y = [class_mapping[x] for x in labels]
+    print(y)
+    X_train, X_test, y_train, y_test, train_class, test_class = preprocessing(image_array, y)
+    print(len(class_mapping))
+
+    augment = False
+
+    # model configuration
+    model = cnn(X_train)
+    model.summary()
+    sgd = SGD(lr=0.01, decay=1e-5, momentum=0.9, nesterov=True)
+    #adam = Adam()
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+
+    X_train = X_train.astype('float32')
+
+    if augment:
+        datagen = ImageDataGenerator(width_shift_range=0.1, height_shift_range=0.1, zoom_range=0.07, fill_mode='nearest', rotation_range=30)
+        datagen.fit(X_train)
+        model.fit_generator(datagen.flow(X_train, y_train, batch_size=20),
+                            steps_per_epoch=X_train.shape[0] // 50,
+                            epochs=1500,
+                            validation_data=(X_test, y_test))
+    else:
+        model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=100, batch_size=20)
+
+    y_preds = model.predict(X_test)
+    np.round(y_preds, 3)
+
+    y_hat = [np.argmax(y) for y in y_preds]  # list of predictions
+    np.array(y_hat)
+
+    # evaluation
+    acc = accuracy_score(test_class, y_hat)
+    print(acc)
+    classes = [x for x in class_mapping.keys()]
+    print(classification_report(test_class, y_hat, target_names = classes))
+
+    model.save('./Model/lp_recognition_noaugment.h5')
+    # model.save_weights('./Model/lp_recognition_weights_noaugment.h5')
+
+if __name__ == '__main__':
+    main()
